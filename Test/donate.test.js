e497dda586fc77bed46f4a5faf6449de8224c12a
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-14 14:48:58 +0545

    database for testing

---
+++ Test/donate.test.js
@@ -0,0 +1,26 @@
+const Donate = require('../models/donateModel');
+
+const mongoose = require('mongoose');
+const donateModel = require('../models/donateModel');
+
+// use the new name of the database
+
+const url = 'mongodb://localhost:27017/foodbank_test';
+
+beforeAll(async () => {
+
+ await mongoose.connect(url, {
+
+ useNewUrlParser: true,
+
+ useCreateIndex: true
+
+ });
+
+});
+
+afterAll(async () => {
+
+ await mongoose.connection.close();
+
+});
