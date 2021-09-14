ec5b4e7b640a5839367efe20600e9358ce69c1c1
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-14 14:52:11 +0545

    testing for insert done

--- Test/donate.test.js
+++ Test/donate.test.js
@@ -24,3 +24,36 @@ afterAll(async () => {
  await mongoose.connection.close();
 
 });
+
+
+describe('Donation Schema test anything', () => {
+
+    // the code below is for insert testing
+    
+     it('Add donation testing anything', () => {
+    
+     const donation = {
+    
+     'donorName': 'Arbin Dhakal',
+     'phone': '9821898001',
+     'country': 'Nepal',
+     'district': 'Kathmandu',
+     'street': 'Putalisadak',
+      'foodtype': 'fresh',
+     'date': '09/09/2021',
+    
+     };
+    
+    
+     return Donate.create(donation)
+    
+     .then((donate_ret) => {
+    
+     expect(donate_ret.donorName).toEqual('Arbin Dhakal');
+    
+     });
+    
+     });
+    
+
+})
