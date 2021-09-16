b6b3f68f407742064106c9a91c6edce892e03421
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-16 12:16:56 +0545

    insert user test done

--- Test/User.test.js
+++ Test/User.test.js
@@ -23,3 +23,31 @@ afterAll(async () => {
  await mongoose.connection.close();
 
 });
+
+describe('User Schema test anything', () => {
+
+    // the code below is for insert testing
+    
+     it('Add User testing anything', () => {
+    
+     const register = {
+    
+     'fullname': 'Arbin Dhakal',
+     'phone': '9821898001',
+     'email': 'arbin@gmail.com',
+     'password': 'arbin123'
+    
+     };
+    
+     
+    
+     return User.create(register)
+    
+     .then((register_ret) => {
+    
+     expect(register_ret.fullname).toEqual('Arbin Dhakal');
+    
+     });
+    
+     });
+    })
