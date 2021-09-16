8a7f605046e7e6713288e453efe93cb33cdbdae9
Arbin Dhakal <arbin.dhakal123@gmail.com>
2021-09-16 11:48:34 +0545

    database for request

---
+++ Test/Request.test.js
@@ -0,0 +1,9 @@
+const Donate = require('../models/donateModel');
+
+const mongoose = require('mongoose');
+const donateModel = require('../models/donateModel');
+
+// use the new name of the database
+
+const url = 'mongodb://localhost:27017/foodbank_test';
+
